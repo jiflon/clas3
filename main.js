788307b2b82730ba467d39fcf8102ab31adff5e8
Jonathan Flores <85594501+jiflon@users.noreply.github.com>
2022-04-09 11:24:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+//COMENTARIOS DE CASLASE 3.-
+//let nombre //variable no completa let nombre = "juan"; este si tiene valor 
+//boolean falso
+//console.log(Boolean(nombre));
+// let nombre = ""
+// let apellido = ""
+//  //&&operador tiene que cumplir con ambos parametros si no seria falso
+// if (nombre && apellido){
+//     console.log("el usuario si tiene apellido")
+// } else {}
+// II or puede tener uno de ambos datos
+
+
+//----CLASE3---USO DE SWITCH 07 0422----------------------------------------------------
+//clase2 //switch
+
+// let instrumento = "trompeta";
+
+// switch (instrumento){
+//     case "bateria":
+//         console.log("yo toco la bateria");
+//        break;
+//        case "guitarra":
+//         console.log("yo toco la guitarra");
+//        break;    
+//        case "trompeta":
+//         console.log("yo toco la trompeta");
+//        break; 
+// }
